321224ed48b7b5323f2e878bf5d85a76da3c151f
Taylor Owens <tsowens1030@gmail.com>
2022-04-10 18:16:41 -0400

    Update Syn6288.h

--- AVR Implementation/Syn6288.h
+++ AVR Implementation/Syn6288.h
@@ -1,83 +1,31 @@
+#ifndef Syn6288_h
+#define Syn6288_h
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
-#include "Syn6288.h"
-#include <stdio.h>
-#include <arduino.h>
+#include <stddef.h>
 
-uint8_t head[5] = {0xfd,0x00,0x00,0x01,0x00};//�ϳɲ�������
-uint8_t sound[5] = {0xfd,0x00,0x00,0x01,0x01};//�ϳ���ʾ��
-uint16_t boud[5]={0xFD,0x00,0x03,0x31,0x00}; //���ò�����
-/*�����ı��ϳ����musicΪ������������*/
-void Syn6288::play(uint8_t *text,uint8_t TEXTLEN,uint8_t music)
-  {
-		pi=0;
-		delay(100);
-  head[2]=TEXTLEN+3;
-   switch(music)
-        {
-          case 0: head[4]=0x01;break;
-          case 1: head[4]=0x09;break;
-          case 2: head[4]=0x11;break;
-          case 3: head[4]=0x19;break;
-          case 4: head[4]=0x21;break;
-          case 5: head[4]=0x29;break; 
-          case 6: head[4]=0x31;break;
-          case 7: head[4]=0x39;break;
-          case 8: head[4]=0x41;break;
-          case 9: head[4]=0x49;break; 
-          case 10: head[4]=0x51;break;
-          case 11: head[4]=0x59;break;
-          case 12: head[4]=0x61;break;
-          case 13: head[4]=0x69;break;
-          case 14: head[4]=0x71;break;
-          case 15: head[4]=0x79;break;
-        } 
-   for(int i=0;i<5;i++)
-      {
-        pi^=head[i];
-        Serial.write(head[i]);
-        delay(2);
-      }
-   for(int j=0;j<TEXTLEN;j++)
-      {
-        pi^=text[j];
-        Serial.write(text[j]);
-        delay(2);
-      }
-    Serial.write(pi);
-    delay(300*TEXTLEN);
-  }
+#define HEADLEN      5
+//#define LEN_OFFSET    2
 
-
-
-void Syn6288::Slaveboudset(uint16_t boudr)  //����������
+//#define uint8_t    unsigned int
+class Syn6288
 {
-	uint8_t p;
-    p=0;
-switch(boudr)
-  {
+  public:
 
-   case 9600:   boud[4]=0x00;
-                   break;
-   case 19200:  boud[4]=0x01;
-                   break;
-  }
-  for(int z=0;z<HEADLEN;z++)
-    {
-      p^=boud[z];
-      Serial.write(boud[z]);
-      delay(2);
-    }               
-  Serial.write(p);
-  p=0;
-  for(int z=0;z<HEADLEN;z++)
-    {
-      p^=boud[z];
-      Serial.write(boud[z]);
-      delay(2);
-    }               
-  Serial.write(p);
-  delay(200);
-}
+//private:
+uint8_t music;
+uint8_t TEXTLEN;
+uint8_t pi;
+void play(uint8_t *text,uint8_t TEXTLEN,uint8_t music);
+//void play(uint8_t *text,uint8_t music);
+void Slaveboudset(uint16_t boudr);
+void stop();
+void restore();
+void inquire();
+void Pause();
+void sleep();
+
+};
+#endif
